b53ff1d5058d90a0b9a9b12e91b3711f9ce82988
Jack Kelly <jack@jackkelly.name>
2018-07-06 08:55:54 +1000

    Remove Functor/Applicative/Monad instance from Proof

--- src/Logic/Proof.hs
+++ src/Logic/Proof.hs
@@ -9,38 +9,35 @@
 -}
 
 module Logic.Proof
-  ( -- * The `Proof` monad
+  ( -- * The `Proof` type
     Proof
   , axiom, sorry
   ) where
 
 {--------------------------------------------------
-  The `Proof` monad
+  The `Proof` type
 --------------------------------------------------}
 
-{-| The @Proof@ monad is used as a domain-specific
+{-| The @Proof@ type is used as a domain-specific
     language for constructing proofs. A value of type
     @Proof p@ represents a proof of the proposition @p@.
 
-    For example, this function constructions a proof of
-    "P or Q" from the assumption "P and Q":
+    A @Proof@ as an argument to a function represents an
+    assumption. For example, this function constructions a proof of "P
+    or Q" from the assumption "P and Q":
 
-> and2or :: (p && q) -> Proof (p || q)
+> and2or :: Proof (p && q) -> Proof (p || q)
 >
-> and2or pq = do
->    p <- and_elimL pq    -- or: "p <- fst <$> and_elim pq"
->    or_introL p
+> and2or pq = introOrL $ elimAndL pq
 
     If the body of the proof does not match the proposition
     you claim to be proving, the compiler will raise a type
     error. Here, we accidentally try to use @or_introR@
     instead of @or_introL@:
 
-> and2or :: (p && q) -> Proof (p || q)
+> and2or :: Proof (p && q) -> Proof (p || q)
 >
-> and2or pq = do
->    p <- and_elimL pq
->    or_introR p
+> and2or pq = introOrR $ elimAndL pq
 
 resulting in the error
 
@@ -48,27 +45,17 @@ resulting in the error
     • Couldn't match type ‘p’ with ‘q’
       ‘p’ is a rigid type variable bound by
         the type signature for:
-          and2or :: forall p q. (p && q) -> Proof (p || q)
+          and2or :: forall p q. Proof (p && q) -> Proof (p || q)
 
       ‘q’ is a rigid type variable bound by
         the type signature for:
-          and2or :: forall p q. (p && q) -> Proof (p || q)
+          and2or :: forall p q. Proof (p && q) -> Proof (p || q)
 
       Expected type: Proof (p || q)
         Actual type: Proof (p || p)
 @
 -}
 data Proof (pf :: *) = QED
-
-instance Functor Proof where
-  fmap _ = const QED -- modus ponens (external?)
-
-instance Applicative Proof where
-  pure = const QED -- axiom
-  pf1 <*> pf2 = QED -- modus ponens (internal?)
-
-instance Monad Proof where
-  pf >>= f = QED
 
 -- | @sorry@ can be used to provide a "proof" of
 --   any proposition, by simply assering it as
